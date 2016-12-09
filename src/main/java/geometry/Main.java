7221bc1cf4abc5adf6baf35b62290088e4e2e19d
Chirkov Boris <b.v.chirkov@gmail.com>
2016-12-09 21:03:13 +0400

    Добавил Copyright

---
+++ src/main/java/geometry/Main.java
@@ -0,0 +1,29 @@
+/**
+ This file is part of SimulationMoving.
+
+ SimulationMoving is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ SimulationMoving is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with SimulationMoving. If not, see <http://www.gnu.org/licenses/>.
+
+ Copyright 2016 Kolodkin Vladimir, Chirkov Boris, Galiullin Marat
+ */
+package geometry;
+
+/**
+ * Created by boris on 09.12.16.
+ */
+public class Main {
+
+    public static void main (String ... args) {
+
+    }
+}
