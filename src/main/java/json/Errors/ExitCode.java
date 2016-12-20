388471dbddfa232aadb7ed05fc33b0db0172be54
Boris Chirkov <b.v.chirkov@udsu.ru>
2016-12-20 09:16:17 +0400

    Added file with exit code
    Планируется помещать в этот файл коды, с которыми программа завершает
    свое выполнение.

---
+++ src/main/java/json/Errors/ExitCode.java
@@ -0,0 +1,11 @@
+package json.Errors;
+
+/**
+ * Created by boris on 20.12.16.
+ */
+public class ExitCode {
+    /**
+     * Файл, который подвергается парсингу пуст
+     */
+    public static final int FILE_EMPTY = 10;
+}
