7410cfc62b3fb591b8e22c8cc513dd7c897a0501
Boris Chirkov <b.v.chirkov@udsu.ru>
2016-12-20 10:01:38 +0400

    Added class Direction
    Класс определяет возможные направления движения по лестнице

---
+++ src/main/java/json/extendetGeometry/Direction.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+ Copyright (C) 2016 Chirkov Boris <b.v.chirkov@udsu.ru>
+
+ Project website:       http://eesystem.ru
+ Organization website:  http://rintd.ru
+
+ --------------------- DO NOT REMOVE THIS NOTICE ------------------------------
+ This file is part of SimulationMoving.
+
+ SimulationMoving is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ SimulationMoving is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with SimulationMoving. If not, see <http://www.gnu.org/licenses/>.
+ ------------------------------------------------------------------------------
+
+ This code is in BETA; some features are incomplete and the code
+ could be written better.
+ *****************************************************************************/
+
+package json.extendetGeometry;
+
+/**
+ * Класс описания направления движения по лестнице.
+ * Created by boris on 13.12.16.
+ */
+public class Direction {
+    /**
+     * Направление движения - вверх
+     */
+    public static final int UP = 1;
+
+    /**
+     * Направление движения - вниз
+     */
+    public static final int DOWN = -1;
+}
