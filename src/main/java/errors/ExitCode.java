d6ae76d78b4d35f079610aae3da00ca153dd322a
Boris Chirkov <b.v.chirkov@udsu.ru>
2016-12-20 12:33:19 +0400

    Move up package errors

---
+++ src/main/java/errors/ExitCode.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Chirkov Boris <b.v.chirkov@udsu.ru>
+ *
+ * Project website:       http://eesystem.ru
+ * Organization website:  http://rintd.ru
+ *
+ * --------------------- DO NOT REMOVE THIS NOTICE -----------------------------
+ * ExitCode is part of jSimulationMoving.
+ *
+ * jSimulationMoving is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jSimulationMoving is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jSimulationMoving. If not, see <http://www.gnu.org/licenses/>.
+ * -----------------------------------------------------------------------------
+ *
+ * This code is in BETA; some features are incomplete and the code
+ * could be written better.
+ ******************************************************************************/
+
+package errors;
+
+/**
+ * Created by boris on 20.12.16.
+ */
+public class ExitCode {
+    /**
+     * Файл, который подвергается парсингу пуст
+     */
+    public static final int FILE_EMPTY = 10;
+}
