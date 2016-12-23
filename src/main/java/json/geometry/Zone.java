ebf79299419f3b7e641d64530695fcced1c6d2c6
Boris Chirkov <b.v.chirkov@udsu.ru>
2016-12-23 10:36:15 +0400

    Reformat and Update

--- src/main/java/json/geometry/Zone.java
+++ src/main/java/json/geometry/Zone.java
@@ -27,12 +27,6 @@
 
 package json.geometry;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
-
 import java.util.ArrayList;
 
 /**
@@ -41,12 +35,12 @@ import java.util.ArrayList;
  * @param <L> Класс, расширенный {@link Light}
  * @param <S> Класс, расширенный {@link Sensor}
  * @param <P> Класс, расширенный {@link Speaker}
+ *
  * @author mag
  */
 public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker> {
-
-    public static final int FLOOR = 0; // Обыное помещение
-    public static final int STAIRS = 1; // Лестничная площадка
+    public static final int FLOOR   = 0; // Обыное помещение
+    public static final int STAIRS  = 1; // Лестничная площадка
     public static final int UNKNOWN = -1; // Неопределенный тип
     /**
      * Идетнификатор зоны в формате UUID. <br>
@@ -56,11 +50,11 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
      * @Example В шестнадцатеричной системе счисления UUID выглядит как:<br>
      * 550e8400-e29b-41d4-a716-446655440000
      */
-    private String id;
+    private String       id;
     /**
      * Высота потолка зоны в метрах
      */
-    private double ceilingHeight;
+    private double       ceilingHeight;
     /**
      * Тип зоны по пожарной нагрузке. Цифровой идентификатор вида пространства.
      * <ol>
@@ -81,19 +75,19 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
      * <li>Торговый зал гипермаркета</li>
      * </ol>
      */
-    private int fireType;
+    private int          fireType;
     /**
      * Количество людей в зоне
      */
-    private double numOfPeople;
+    private double       numOfPeople;
     /**
      * Примечание. Дополнительная информация.
      */
-    private String note;
+    private String       note;
     /**
      * Тип зоны
      */
-    private String type;
+    private String       type;
     /**
      * Геометрия зоны.<br>
      * [кольца][точки][x,y,z]
@@ -115,10 +109,9 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
     protected Zone() {
     }
 
-    @Override
-    public String toString() {
-        return "Zone: {" + "id='" + id + '\'' + ", ceilingHeight=" +
-                ceilingHeight + ", fireType=" + fireType + ", numOfPeople="
+    @Override public String toString() {
+        return "Zone: {" + "id='" + id + '\'' + ", ceilingHeight="
+                + ceilingHeight + ", fireType=" + fireType + ", numOfPeople="
                 + numOfPeople + ", note='" + note + '\'' + ", type='" + type
                 + '\'' + ", lights=" + lights + ", sensors=" + sensors + ", "
                 + "speakers=" + speakers + '}';
@@ -127,82 +120,37 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
     /**
      * Метод возвращает тип зоны, который задается на этапе ввода здания
      *
-     * @return 0, если зона имеет тип FLOOR <br>
-     * 1, если зона имеет тим STAIRS <br>
-     * -1, если тип зоны не определен (UNKNOWN)
+     * @return 0, если зона имеет тип FLOOR <br> 1, если зона имеет тим STAIRS
+     * <br> -1, если тип зоны не определен (UNKNOWN)
      */
     public int getType() {
         switch (type) {
-            case "FLOOR":
-                return FLOOR;
-            case "STAIRS":
-                return STAIRS;
-            default:
-                return UNKNOWN;
+        case "FLOOR":
+            return FLOOR;
+        case "STAIRS":
+            return STAIRS;
+        default:
+            return UNKNOWN;
         }
     }
 
     /**
-     * Перепад высот в пределах зоны
+     * Позволяет установить тип зоны.
+     *
+     * @param type тип зоны: {@link Zone#FLOOR} или {@link Zone#STAIRS}
      */
-    public double getHeightDifference() {
-        double zMin = Double.MAX_VALUE;
-        double zMax = -Double.MIN_VALUE;
-        for (int i = 0; i < getXyz().length; i++) {
-            final double z = getXyz(0, i, 2);
-            if (z >= zMax) zMax = z;
-            if (z <= zMin) zMin = z;
+    public void setType(final int type) {
+        switch (type) {
+        case FLOOR:
+            this.type = "FLOOR";
+            break;
+        case STAIRS:
+            this.type = "STAIRS";
+            break;
+        default:
+            this.type = "UNKNOWN";
+            break;
         }
-        return zMax - zMin;
-    }
-
-    /**
-     * Приватный метод, возвращающий полигон на структуре XY
-     */
-    private Polygon getPolygon() {
-        if (xyz == null)
-            return null; // если геометрии нет, то и в полигон не превратить
-        GeometryFactory mGF = new GeometryFactory();
-        // переструктурируем геометрию колец в Coordinate[][]
-        // внешнее кольцо
-        Coordinate[] geomOut = new Coordinate[xyz[0].length];
-        for (int l = 0; l < xyz[0].length; l++) {
-            geomOut[l] = new Coordinate(xyz[0][l][0], xyz[0][l][1]);
-        }
-        // внутренние кольца
-        LinearRing[] internalRings = null;
-        Coordinate[][] geomInt;
-        if (xyz.length >= 2) { // если внутренние кольца есть
-            geomInt = new Coordinate[xyz.length - 1][];
-            internalRings = new LinearRing[xyz.length - 1];
-            for (int k = 1; k < xyz.length; k++) { // Начиная с первого кольца (не с нулевого)
-                geomInt[k - 1] = new Coordinate[xyz[k].length];
-                for (int l = 0; l < xyz[k].length; l++) {
-                    geomInt[k - 1][l] = new Coordinate(xyz[k][l][0], xyz[k][l][1]);
-                }
-                internalRings[k - 1] = new LinearRing(new CoordinateArraySequence(geomInt[k - 1]), mGF);
-            } // for
-        } // if
-
-        return new Polygon(new LinearRing(new CoordinateArraySequence(geomOut), mGF), internalRings, mGF);
-    }
-
-    /**
-     * @return Площадь зоны
-     */
-    public double getArea() {
-        Polygon mP = getPolygon();
-        if (mP != null) return mP.getArea();
-        else return 0;
-    }
-
-    /**
-     * @return Периметр зоны
-     */
-    public double getPerimeter() {
-        Polygon mP = getPolygon();
-        if (mP != null) return mP.getLength();
-        else return 0;
     }
 
     /**
@@ -306,6 +254,7 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
      * @param i - кольца
      * @param j - точки
      * @param k - x,y,z
+     *
      * @return Трехмерный массив, описывающий геометрию зоны.
      */
     public double getXyz(int i, int j, int k) {
@@ -320,8 +269,8 @@ public abstract class Zone<L extends Light, S extends Sensor, P extends Speaker>
     }
 
     /**
-     * @return Список устройств мониторинга состояния среды, которые
-     * находятся в данной зоне
+     * @return Список устройств мониторинга состояния среды, которые находятся в
+     * данной зоне
      */
     public ArrayList<S> getSensors() {
         return sensors;
