4a5e77f5e1016199c71830b55186726311605b81
Boris Chirkov <b.v.chirkov@udsu.ru>
2017-01-25 13:48:05 +0400

    Added use FDS data

--- src/main/java/simulation/Moving.java
+++ src/main/java/simulation/Moving.java
@@ -1,5 +1,5 @@
-/******************************************************************************
- Copyright (C) 2016 Kolodkin Vladimir
+/*
+ Copyright (C) 2016 Kolodkin Vladimir, Chirkov Boris
 
  Project website:       http://eesystem.ru
  Organization website:  http://rintd.ru
@@ -19,14 +19,25 @@
 
  You should have received a copy of the GNU Lesser General Public License
  along with SimulationMoving. If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+*/
 
 package simulation;
 
+import fds.DevcHelper;
+import fds.ReadFDSOutput;
 import json.extendetGeometry.BIMExt;
 import json.extendetGeometry.BIMLoader;
+import json.extendetGeometry.SensorExt;
+import json.extendetGeometry.ZoneExt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static json.extendetGeometry.SensorExt.T_SMOKE;
+import static json.extendetGeometry.SensorExt.T_TEMPERATURE;
 
 /**
  * Класс моделирования
@@ -35,12 +46,14 @@ public class Moving extends Thread {
     private static final Logger log = LoggerFactory.getLogger(Moving.class);
 
     @Override public void run() {
+        log.info("Running thread with simulation moving");
+
         // Загрузка структуры здания BIM
         ClassLoader thisClassLoader = Moving.class.getClassLoader();
         BIMLoader<BIMExt> bimLoader = new BIMLoader<>(thisClassLoader.
                 getResourceAsStream("Stand-v1.2.json"), BIMExt.class);
 
-        log.info("Running thread with simulation moving");
+        LinkedHashMap<Double, ArrayList<DevcHelper>> fdsData = ReadFDSOutput.readDevc("UdSU_devc.csv");
 
         BIMExt bim = bimLoader.getBim();
         Traffic traffic = new Traffic(bim);
@@ -49,21 +62,43 @@ public class Moving extends Thread {
         log.info("Set max cycle index {}", acceptRepeat);
 
         double timeModel = 0.0; // Текущее время моделирования эвакуации, c
-        double time = 6; // Интервал моделирования эвакуации, c
+        double time; // Интервал моделирования эвакуации, c
 
-        for (int i = 0; i < acceptRepeat; i++) {
-            int balance = traffic.footTraffic(time);
+        double previousFdsTime = 0.0;
+        /*for (int i = 0; i < acceptRepeat; i++) {*/
+        for (Map.Entry<Double, ArrayList<DevcHelper>> d : fdsData.entrySet()) {
+            double fdsTime = d.getKey();
+
+            for (ZoneExt ze : bim.getZones().values()) // по зонам
+                for (SensorExt se : ze.getSensors()) // по сенсорам в зоне
+                    for (DevcHelper dh : d.getValue()) // по значениям
+                        if (se.getId().equalsIgnoreCase(dh.getId())) { // нашли сенсор для которого есть данные
+                            if (se.isSmoke() && dh.getType() == T_SMOKE) {
+                                se.setVisible(dh.getValue());
+                                break;
+                            }
+                            if (se.isTemperature() && dh.getType() == T_TEMPERATURE) {
+                                se.setTemperature(dh.getValue());
+                                break;
+                            }
+                        }
+
+            time = fdsTime - previousFdsTime;
+            previousFdsTime = fdsTime;
             timeModel += time;
+
+            int balance = traffic.footTraffic(time);
 
             log.info("In progress: number of people in Safety zone: {}, simulation time: {}",
                     bim.getSafetyZone().getNumOfPeople(), timeModel);
 
             if (balance != -1) {
-                timeModel += balance * time;
+                log.debug("fdsTome: {}", fdsTime);
+                //timeModel += balance * fdsTime;
                 break;
             }
 
-            try { sleep(500L); } catch (InterruptedException e) {e.printStackTrace();}
+            /*try { sleep(500L); } catch (InterruptedException e) {e.printStackTrace();}*/
         }
 
         log.info("Successful finish simulation. Total: number of people in Safety zone: {}, simulation time: {}",
